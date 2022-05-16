251a6fc07d46ce15c1bf9c5cd5a47f266cb3bff3
Tarcísio Eduardo Moreira Crocomo <tarcisioe@pm.me>
2022-05-16 02:55:33 -0300

    Implement database diffing.

--- songhash.py
+++ songhash.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 import hashlib
 import sys
-from dataclasses import dataclass
+from enum import Enum
+from dataclasses import dataclass, replace
 from pathlib import Path
 from typing import Sequence, Mapping
 from threading import Thread
@@ -37,7 +38,7 @@ class Song:
             return Song(file_data.path, f.read(), number, file_data.timestamp_ns)
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, order=True)
 class SongHash:
     path: Path
     sha256: str
@@ -55,7 +56,24 @@ class SongHash:
 @dataclass
 class Database:
     base_directory: Path
-    hashes: Mapping[Path, SongHash]
+    hashes: dict[Path, SongHash]
+
+    def is_newer_than_recorded(self, song_file_data: SongFileData) -> int:
+        song_hash = self.hashes.get(song_file_data.path.relative_to(self.base_directory))
+
+        if song_hash is None:
+            return True
+
+        return song_file_data.timestamp_ns > song_hash.timestamp_ns
+
+    def matches_recorded_hash(self, song_hash: SongHash) -> bool:
+        return self.hashes[song_hash.path].sha256 == song_hash.sha256
+
+    def update(self, new_hashes: Sequence[SongHash]) -> None:
+        self.hashes.update(
+            ((path := s.path.relative_to(self.base_directory)), replace(s, path=path))
+            for s in new_hashes
+        )
 
 
 def read_songs_into_queue(song_file_data: Sequence[SongFileData], q: Queue[Song | None]) -> None:
@@ -86,12 +104,9 @@ def hash_songs(song_file_data: Sequence[SongFileData]) -> list[SongHash]:
     return hash_songs_from_queue(q, expected_total=len(song_file_data))
 
 
-def get_old_database(filename: Path) -> Database | None:
+def read_database(filename: Path) -> Database | None:
     if not filename.exists():
         return None
-
-    print("Previous data exists. Updating...")
-
 
     with filename.open("r") as f:
         header, *file_lines = (l.strip() for l in f.readlines())
@@ -102,7 +117,7 @@ def get_old_database(filename: Path) -> Database | None:
         return Database(
             base_directory,
             {
-                (path := base_directory / file): SongHash(path, sha256, int(timestamp_ns))
+                (path := Path(file)): SongHash(path, sha256, int(timestamp_ns))
                 for file, sha256, timestamp_ns in
                 split_lines
             }
@@ -110,7 +125,7 @@ def get_old_database(filename: Path) -> Database | None:
 
 
 def get_timestamp_from_database(database: Database, song_path: Path) -> int:
-    song_hash = database.hashes.get(song_path)
+    song_hash = database.hashes.get(song_path.relative_to(database.base_directory))
 
     if song_hash is None:
         return 0
@@ -118,50 +133,75 @@ def get_timestamp_from_database(database: Database, song_path: Path) -> int:
     return song_hash.timestamp_ns
 
 
-def get_songs_to_update(songs_pre_check: Sequence[SongFileData], old_database: Database | None) -> list[SongFileData]:
-    if old_database is None:
-        return list(songs_pre_check)
-
+def get_songs_to_update(songs_pre_check: Sequence[SongFileData], old_database: Database) -> list[SongFileData]:
     return [
         s
         for s in songs_pre_check
-        if s.timestamp_ns > get_timestamp_from_database(old_database, s.path)
+        if old_database.is_newer_than_recorded(s)
     ]
 
 
-def merge(old_database: Database | None, new_hashes: Sequence[SongHash]) -> list[SongHash]:
-    if old_database is None:
-        return list(new_hashes)
-
-    database = dict(old_database.hashes)
-    database.update((s.path, s) for s in new_hashes)
-    return list(database.values())
-
-
-def output(directory: Path, hashes: Sequence[SongHash], filename: Path) -> None:
-    hashes = sorted(hashes, key=lambda h: h.path)
+def output(database: Database, filename: Path) -> None:
+    hashes = sorted(database.hashes.values())
 
     with open(filename, "w") as f:
-        print(directory, file=f)
+        print(database.base_directory, file=f)
         for song_hash in hashes:
-            print(f'{song_hash.path.relative_to(directory)}\t{song_hash.sha256}\t{song_hash.timestamp_ns}', file=f)
+            print(f'{song_hash.path}\t{song_hash.sha256}\t{song_hash.timestamp_ns}', file=f)
+
+
+@dataclass
+class Diff:
+    added: list[Path]
+    removed: list[Path]
+    modified: list[Path]
+
+
+def diff_databases(older: Database, newer: Database) -> Diff:
+    added: list[Path] = []
+    removed: list[Path] = []
+    modified: list[Path] = []
+
+    for path, song_hash in older.hashes.items():
+        if path not in newer.hashes:
+            removed.append(path)
+            continue
+
+        if not newer.matches_recorded_hash(song_hash):
+            modified.append(path)
+
+    for path in newer.hashes:
+        if path not in older.hashes:
+            added.append(path)
+
+    return Diff(added, removed, modified)
+
+
+def ensure_database(base_directory: Path, filename: Path) -> Database:
+    database = read_database(filename)
+
+    if database is None:
+        return Database(base_directory, {})
+
+    if database.base_directory != base_directory:
+        print("Cowardly refusing to update a database for a different directory.", file=sys.stderr)
+        raise typer.Exit(1)
+
+    print("Previous data exists. Updating...")
+    return database
 
 
 @APP.command()
-def main(directory: Path, filename: Path, base_directory: Path | None = None) -> None:
+def scan(directory: Path, filename: Path, base_directory: Path | None = None) -> None:
     base_directory = directory if base_directory is None else base_directory
 
     base_directory = base_directory.absolute()
     directory = directory.absolute()
 
-    song_paths = [SongFileData(s, s.stat().st_mtime_ns) for s in Path(sys.argv[1]).rglob('*') if s.is_file() and s.suffix in ('.mp3', '.m4a')]
-    old_database = get_old_database(filename)
+    database = ensure_database(base_directory, filename)
 
-    if old_database is not None and old_database.base_directory != base_directory:
-        print("Cowardly refusing to update a database for a different directory.", file=sys.stderr)
-        raise typer.Exit(1)
-
-    songs_to_update = get_songs_to_update(song_paths, old_database)
+    song_paths = [SongFileData(s, s.stat().st_mtime_ns) for s in directory.rglob('*') if s.is_file() and s.suffix in ('.mp3', '.m4a')]
+    songs_to_update = get_songs_to_update(song_paths, database)
 
     if not songs_to_update:
         print("No new songs to update. Done!")
@@ -169,7 +209,36 @@ def main(directory: Path, filename: Path, base_directory: Path | None = None) ->
 
     new_hashes = hash_songs(songs_to_update)
 
-    output(base_directory, merge(old_database, new_hashes), filename)
+    database.update(new_hashes)
+
+    output(database, filename)
+
+
+@APP.command()
+def diff(older_path: Path, newer_path: Path) -> None:
+    older = read_database(older_path)
+
+    if older is None:
+        raise typer.Exit(1)
+
+    newer = read_database(newer_path)
+
+    if newer is None:
+        raise typer.Exit(1)
+
+    diff = diff_databases(older, newer)
+
+    print("Added:")
+    for path in diff.added:
+        print(path)
+    print()
+    print("Removed:")
+    for path in diff.removed:
+        print(path)
+    print()
+    print("Modified:")
+    for path in diff.modified:
+        print(path)
 
 
 if __name__ == '__main__':
